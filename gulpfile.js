32ba0a53c8e6c2df9d9dbe851a3ee26b00c2dd31
CodeLittlePrince <1006312908@qq.com>
2017-05-01 01:58:53 +0800

    update to gulp4.0 because of the fucking async

--- gulpfile.js
+++ gulpfile.js
@@ -5,63 +5,91 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     autoprefixer = require('autoprefixer'),
     postcss = require('gulp-postcss'),
-    clean = require('gulp-clean-old'),
+    del = require('del'),
     rev = require('gulp-rev'),
-    gulpSequence = require('gulp-sequence');
+    revCollector = require('gulp-rev-collector'),
+    srcReplace = require('gulp-replace-src');
 
 // Scripts Tasks
 // 1.uglify
-gulp.task('scripts', function(){
-    gulp.src('js/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('build/js'));
-});
+// 2.generate mainfest
+function scripts(){
+    return gulp.src('js/*.js')
+        .pipe(uglify())
+        .pipe(rev())
+        .pipe(gulp.dest('build/js'))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('rev/js'));
+}
 
 // Styles Tasks
 // 1.compile scss
 // 2.compress css
 // 3.add prefix
 // 4.add sourcemaps
-gulp.task('styles', function(){
-    sass('scss/**/*.scss', {
+// 5.generate mainfest
+function styles(){
+    return sass('scss/**/*.scss', {
         style: 'compressed',
         sourcemap: true
     })
     .on('error', sass.logError)
     .pipe(postcss([autoprefixer()]))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('build/css'));
-});
+    .pipe(rev())
+    .pipe(gulp.dest('build/css'))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('rev/css'));
+}
 
 // Images Tasks
 // 1.compress images
-gulp.task('images', function(){
-    gulp.src('img/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('build/img'))
-});
+// 2.generate mainfest
+function images(){
+    return gulp.src('img/*')
+        .pipe(imagemin())
+        .pipe(rev())
+        .pipe(gulp.dest('build/img'))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('rev/img'));
+}
 
 // Clean Resource For "add resource hash version"
-gulp.task('clean', function(){
-    gulp.src('build/**/*.*', {read: false})
-    .pipe(clean());
-});
-// Add Resource Hash Version
-gulp.task('version', function(){
-    gulp.src(['build/js/*.js', 'build/css/*.css', 'build/img/*'])
-    .pipe(rev())
-    .pipe(rev.manifest())
-    .pipe(gulp.dest('build/'));
-});
+function clean(){
+    return del(['build/**/*', 'rev/**/*']);
+}
+
+// Genetate Files With Hash Suffix & Replace Link In HTML
+function replace() {
+    return gulp.src(['rev/**/*.json','index.html'])
+        .pipe(revCollector({
+            replaceReved: true,  //模板中已经被替换的文件是否还能再被替换,默认是false
+            dirReplacements: {   //标识目录替换的集合, 因为gulp-rev创建的manifest文件不包含任何目录信息,
+                'css/': 'css/',
+                'js/': 'js/'
+            }
+        }))
+        .pipe(gulp.dest('build'));
+}
 
 // Watch Tasks
 // 1.watch javascript
 // 2.watch scss
-gulp.task('watch', function(){
-    gulp.watch('js/*.js', ['scripts']);
-    gulp.watch('scss/**/*.scss', ['styles']);
-})
+function watch(){
+    gulp.watch('js/*.js', scripts);
+    gulp.watch('scss/**/*.scss', styles);
+}
 
-gulp.task('default', ['scripts', 'styles', 'watch']);
+exports.clean = clean;
+exports.scripts = scripts;
+exports.styles = styles;
+exports.images = images;
+exports.watch = watch;
 
-gulp.task('build', gulpSequence('clean', ['scripts', 'styles', 'images'], 'version'));
+gulp.task('default',  gulp.parallel(scripts, styles, watch));
+
+gulp.task('build', gulp.series(
+    clean,
+    gulp.parallel(scripts, styles, images),
+    replace
+));
